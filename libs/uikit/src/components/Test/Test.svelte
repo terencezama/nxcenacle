f4d9541abd599de4e6b1584c399f5fce7f912874
Terence Zama <terence.zama@gmail.com>
2022-10-01 01:31:38 +0400

    component creation ready

--- libs/uikit/src/components/Test/Test.svelte
+++ libs/uikit/src/components/Test/Test.svelte
@@ -1,1 +1,9 @@
-<div class="bg-green-500">testing .. working</div>
+<script>
+  export let rounded = false;
+</script>
+
+<button
+  class={[rounded ? 'rounded-lg' : '', 'px-4', 'bg-yellow-500', 'py-2'].join(
+    ' '
+  )}>cool</button
+>
