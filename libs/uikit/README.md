4a19b7fd4ed54ab1945ed0584530c063dbacbbd2
Terence Zama <terence.zama@gmail.com>
2022-09-30 11:06:33 +0400

    added button component library

---
+++ libs/uikit/README.md
@@ -0,0 +1,3 @@
+# Uikit
+
+Generated with [@nxtensions/astro](https://github.com/nxtensions/nxtensions/tree/main/packages/astro).
---
+++ libs/uikit/src/index.js
@@ -0,0 +1,5 @@
+export { default as Uikit } from './lib/Uikit.astro';
+
+import Check from './lib/check/Check.astro';
+import Button from './lib/Button.svelte';
+export { Check, Button };
